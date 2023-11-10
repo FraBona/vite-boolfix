7249d7192a1769d0b7bc4e1ad2e94a713f606445
FraBona <francescobonafede08@gmail.com>
2023-11-10 14:46:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return{
+        API_KEY: 'c42a3371088d04eb3ec89d322d1ee0db',
+        query: 'ritorno al futuro',
+      }
+    },
+    methods: {
+      fetchMovies( ){
+        axios.get('https://api.themoviedb.org/3/search/movie',{
+          params: {
+            api_key: this.API_KEY,
+            query: this.query
+          }
+        }).then(res => {
+          console.log(res.data);
+        })
+      }
+    }
+  }
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>

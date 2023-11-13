86454bc1a7663eaac9eca945aaf0255b0de1a6a1
FraBona <francescobonafede08@gmail.com>
2023-11-13 11:24:04 +0100

    add flags

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,8 +1,20 @@
 <script>
 
+import { store } from '../store.js'
+
 export default{
   props: {
     item: Object,
+  },
+  data(){
+    return{
+      store: store,
+    }
+  },
+  computed: {
+    srcFlag(){
+      return this.store.flags[this.item.original_language];
+    }
   }
 }
 
@@ -14,11 +26,13 @@ export default{
     <ul>
       <li>{{  item.title }}</li>
       <li>{{ item.original_title }}</li>
-      <li>{{ item.original_language }}</li>
+      <li>
+        <img height="15" v-if="srcFlag" :src="srcFlag" alt="">
+        <p v-else>{{ item.original_language }}</p>
+      </li>
       <li>{{ item.vote_average }}</li>
     </ul>
   </div>
-  
 </template>
 
 
--- src/store.js
+++ src/store.js
@@ -4,5 +4,11 @@ export const store = reactive({
   searchText: '',
   movies: [],
   series: [],
-  API_KEY: 'c42a3371088d04eb3ec89d322d1ee0db'
+  API_KEY: 'c42a3371088d04eb3ec89d322d1ee0db',
+  flags: {
+    it: '/it.png',
+    en: '/en.webp',
+    fr: '/fr.svg.png',
+    de: '/de.png'
+  },
 })
